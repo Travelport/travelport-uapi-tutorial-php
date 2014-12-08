304ca4bf517f01300d08576c7291e10876d7fc83
vivekjyotipramanik <vivekjyotipramanik@users.noreply.github.com>
2014-12-08 16:41:43 -0700

    Update phpSample_Air.php
    Added the parsing code to capture the results instead of using curl_exec output.

--- Air/phpSample_Air.php
+++ Air/phpSample_Air.php
@@ -14,7 +14,9 @@
 * This code is for illustration purposes only. 
 */
 $TARGETBRANCH = 'Enter the Target Branch that you received in your Welcome letter';
-$CREDENTIALS = 'Universal API/API1234567:Password provieded in the welcome leter'; 
+$CREDENTIALS = 'Universal API/API1234567:Password provieded in the welcome leter';
+$Provider = 'ACH'; // Any provider you want to use like 1G/1P/1V/ACH
+
 $message = <<<EOM
 <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
    <soapenv:Header/>
@@ -33,13 +35,16 @@ $message = <<<EOM
          </air:SearchAirLeg>
          <air:AirSearchModifiers>
             <air:PreferredProviders>
-               <com:Provider Code="ACH"/>
+               <com:Provider Code="$Provider"/>
             </air:PreferredProviders>
          </air:AirSearchModifiers>
       </air:AvailabilitySearchReq>
    </soapenv:Body>
 </soapenv:Envelope>
 EOM;
+
+$file = "001-".$Provider."_AirAvailabilityReq.xml"; // file name to save the request xml for test only(if you want to save the request/response)
+prettyPrint($message,$file);//call function to pretty print xml
 
 $auth = base64_encode("$CREDENTIALS"); 
 $soap_do = curl_init ("https://americas.universal-api.pp.travelport.com/B2BGateway/connect/uAPI/AirService");
@@ -58,10 +63,31 @@ curl_setopt($soap_do, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($soap_do, CURLOPT_SSL_VERIFYHOST, false); 
 curl_setopt($soap_do, CURLOPT_POST, true ); 
 curl_setopt($soap_do, CURLOPT_POSTFIELDS, $message); 
-curl_setopt($soap_do, CURLOPT_HTTPHEADER, $header); 
+curl_setopt($soap_do, CURLOPT_HTTPHEADER, $header);
+curl_setopt($soap_do, CURLOPT_RETURNTRANSFER, true); // this will prevent the curl_exec to return result and will let us to capture output
 $return = curl_exec($soap_do);
-print '<br>';
-echo $return;
-print '<br>';
-print_r(curl_getinfo($soap_do));
+
+$file = "001-".$Provider."_AirAvailabilityRsp.xml"; // file name to save the response xml for test only(if you want to save the request/response)
+prettyPrint($return,$file);
+//print '<br>';
+//echo $return;
+//print '<br>';
+//print_r(curl_getinfo($soap_do));
+
+//Pretty print XML
+function prettyPrint($result,$file){
+	$dom = new DOMDocument;
+	$dom->preserveWhiteSpace = false;
+	$dom->loadXML($result);
+	$dom->formatOutput = true;		
+	//call function to write request/response in file	
+	outputWriter($file,$dom->saveXML());
+	return $dom->saveXML();
+}
+
+//function to write output in a file
+function outputWriter($file,$content){	
+	file_put_contents($file, $content); // Write request/response and save them in the File
+}
+
 ?>
